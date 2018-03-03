72ceec8c5faedb5efa014767b87c1e17c9bf1f92
tahaygun <tahaygun@gmail.com>
2018-03-03 20:16:28 +0100

    registiration form arranged

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,5 +2,6 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'process';
 $route['joinpage'] = 'process/openjoinpage';
+$route['register'] = 'process/register';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
