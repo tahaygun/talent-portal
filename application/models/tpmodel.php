338fc3602a3320b2fd39991936d0741227aac5ad
tahaygun <tahaygun@gmail.com>
2018-03-03 18:14:47 +0100

    homepage clean and printed

--- application/models/tpmodel.php
+++ application/models/tpmodel.php
@@ -7,7 +7,8 @@ class Tpmodel extends CI_Model
         $query = "SELECT * FROM users
         JOIN postings
         ON users.id = postings.user_id
-        WHERE postings.highlighted=1";
+        WHERE postings.highlighted=1
+        ORDER BY postings.id DESC";
         $result = $this->db->query($query)->result_array();
         return $result;
     }
